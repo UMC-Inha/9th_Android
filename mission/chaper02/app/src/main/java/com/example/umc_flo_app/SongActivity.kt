8905548e06b7cc2bac4689c6c240d28a0caeb9b5
김용승 <yongyong213@naver.com>
2025-09-23 22:22:22 +0900

    mission: SongActivity, homeFragment구현

---
+++ mission/chaper02/app/src/main/java/com/example/umc_flo_app/SongActivity.kt
@@ -0,0 +1,50 @@
+package com.example.umc_flo_app
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.umc_flo_app.databinding.ActivitySongBinding
+
+class SongActivity : AppCompatActivity(){
+    lateinit var binding: ActivitySongBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySongBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val title = intent.getStringExtra("title")
+        val singer = intent.getStringExtra("singer")
+
+        binding.songTitleTv.text = title
+        binding.songSingerTv.text = singer
+
+        binding.songBtnDownIv.setOnClickListener {
+            finish()
+        }
+
+        binding.songBtnPlayIv.setOnClickListener {
+            Log.d("", "버튼이 클릭되었습니다!")
+            setPlayerStatus(false)
+        }
+
+        binding.songBtnPauseIv.setOnClickListener {
+            setPlayerStatus(true)
+        }
+    }
+
+    fun setPlayerStatus(isPlaying: Boolean){
+        if(isPlaying){
+            binding.songBtnPlayIv.visibility = View.VISIBLE
+            binding.songBtnPauseIv.visibility = View.GONE
+        }
+        else{
+            binding.songBtnPlayIv.visibility = View.GONE
+            binding.songBtnPauseIv.visibility = View.VISIBLE
+        }
+    }
+}
